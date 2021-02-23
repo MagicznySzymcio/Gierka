8ed207c882bbf00a415ad92607ec4923ee82b50c
thesz <theszymon450@gmail.com>
2021-02-23 14:12:36 +0100

    Revert "Revert "Merge branch 'master' into Szymon""
    This reverts commit 8fd020be73ba4b2472bde8c8bf192727aad598ed.

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    public GameObject text;
+    public GameObject player;
+    public GameObject button;
+    public void Start()
+    {
+        text.SetActive(false);
+        button.SetActive(false);
+    }
+
+    public void OnGameEnd()
+    {
+        text.GetComponent<Text>().text = "Przegrales";
+        text.SetActive(true);
+        player.transform.position = new Vector2(0,5);
+        player.SetActive(false);
+        button.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
