14ab93b9e4973aea37462962a293cc5c0c689b41
thesz <theszymon450@gmail.com>
2021-02-24 11:26:39 +0100

    PreDzida

---
+++ Assets/Scripts/ShowCenterOfMass.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCenterOfMass : MonoBehaviour
+{
+    public float Force;
+    public GameObject DzidaObject;
+
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject Dzida = Instantiate(DzidaObject, transform.position, transform.rotation);
+        Dzida.GetComponent<Rigidbody2D>().AddForce(transform.right * Force);
+    }
+}
---
+++ Assets/Scripts/TestMass.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMass : MonoBehaviour
+{
+    Rigidbody2D rb;
+    bool hasHit = false;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasHit == false)
+        {
+            Test();
+        }
+    }
+
+    void Test()
+    {
+        Vector2 direction = rb.velocity;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        hasHit = true;
+    }
+}
