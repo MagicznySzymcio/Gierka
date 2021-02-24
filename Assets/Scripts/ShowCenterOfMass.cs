9f7635d3e5be98fc10c9f2608b6b751b391e3b0d
Angel <ppaluch099@gmail.com>
2021-02-24 14:14:12 +0100

    commit

--- Assets/Scripts/ShowCenterOfMass.cs
+++ Assets/Scripts/ShowCenterOfMass.cs
@@ -6,11 +6,7 @@ public class ShowCenterOfMass : MonoBehaviour
 {
     public float Force;
     public GameObject DzidaObject;
-
-    void Start()
-    {
-
-    }
+    public GameObject Player;
 
     private void Update()
     {
@@ -19,10 +15,15 @@ public class ShowCenterOfMass : MonoBehaviour
             Shoot();
         }
     }
-
     void Shoot()
     {
+
+        Vector3 mousePlacement = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mousePlacement);
+
         GameObject Dzida = Instantiate(DzidaObject, transform.position, transform.rotation);
-        Dzida.GetComponent<Rigidbody2D>().AddForce(transform.position * Force);
+        Dzida.GetComponent<Rigidbody2D>().AddForce(new Vector2(
+                                                        castPoint.origin.x - Player.transform.position.x,
+                                                        castPoint.origin.y - Player.transform.position.y) * Force);
     }
 }
