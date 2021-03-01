4f9eab19cd8a0b8f1caa7e6cb7228fa31a377a83
Angel <ppaluch099@gmail.com>
2021-03-01 13:18:51 +0100

    Fade Out

--- Assets/Scripts/DzidaList.cs
+++ Assets/Scripts/DzidaList.cs
@@ -5,22 +5,40 @@ using UnityEngine;
 public class DzidaList : MonoBehaviour
 {
     public static int liczbaDzid = 0;
-    [SerializeField] private static int maxDzid = 15;
+    [SerializeField] private static int maxDzid = 3;
     private static Queue<GameObject> listaDzid = new Queue<GameObject>();
+    public static SpriteRenderer spriteToFade;
+    static public DzidaList instance;
 
-
+    private void Awake()
+    {
+        instance = this;
+    }
     public static void DzidaUpdate(GameObject dzida)
     {
         liczbaDzid++;
         listaDzid.Enqueue(dzida);
-        
-        if (liczbaDzid>maxDzid)
+
+        if (liczbaDzid > maxDzid)
         {
-            Destroy(listaDzid.Dequeue());
+            GameObject go = listaDzid.Dequeue();
+            spriteToFade = go.GetComponent<SpriteRenderer>();
+            instance.StartCoroutine("FadeOut");
+            
         }
 
         //Debug.Log(liczbaDzid);        
     }
 
+    IEnumerator FadeOut()
+    {
+        for (float f = 1f; f >= -0.05f; f-= 0.05f)
+        {
+            Color c = spriteToFade.material.color;
+            c.a = f;
+            spriteToFade.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
 
 }
