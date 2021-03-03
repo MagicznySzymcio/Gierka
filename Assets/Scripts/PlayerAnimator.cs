b2717a2309615babd01b5b077761a5c38b106143
thesz <theszymon450@gmail.com>
2021-03-03 11:10:58 +0100

    Patryk zmergowany

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class PlayerAnimator : MonoBehaviour
 {
     [SerializeField] private Sprite[] frameArray;
+    [SerializeField] private float frameLength = 1f;
     private int currentFrame;
     private float timer;
 
-    // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
-        if (timer >= 1f)
+        if (timer >= frameLength)
         {
             timer -= 1f;
             currentFrame = (currentFrame + 1) % frameArray.Length;
